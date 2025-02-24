69a07b3f13887082a0b343830d56dc39cbcc18f9
Hamid Ranjbar <123825536+ranjbar-hamid@users.noreply.github.com>
2025-02-24 10:15:21 +0330

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+export default async function Home() {
+  const f = await fetch(
+    "https://script.google.com/macros/s/AKfycbz2ilrGauIAJoWzpZ3IcBv6gEato1hUWcC5p-1OJctJZcmwSIiLvrA5n5c8QZ9E5Mb3/exec?t=list"
+  );
+
+  const data = await f.json();
+  return (
+    <div>
+      {data?.map((i, j) => (
+        <p key={j}>{i.title}</p>
+      ))}
+    </div>
+  );
+}
